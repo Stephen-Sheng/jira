1cf43a658d3c702c40bb1fd21d8f8a0bc753b67f
YutongSheng <544518449@qq.com>
2022-12-01 11:45:36 +0800

    feat: 添加工具防抖与useMount

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import { useState, useEffect } from "react"
-import { cleanObject } from "utils"
+import { cleanObject, useDebounce, useMount } from "utils"
 import * as qs from "qs"
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -13,22 +13,23 @@ export const ProjectListScreen = () => {
     })
     const [list, setList] = useState([])
     const [users,setUsers] = useState([])
+    const debouncedParam = useDebounce(param,500)
 
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async res => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async res => {
             if(res.ok){
                 setList(await res.json())
             }
         })
-    },[param])
+    },[debouncedParam])
 
-    useEffect(() => {
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async res => {
             if(res.ok){
                 setUsers(await res.json())
             }
         })
-    },[])
+    })
     return <div>
         <SearchPanel param = {param} setParam = {setParam} users = {users} />
         <List list = {list} users = {users}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 export const cleanObject = (obj) => {
   let result = { ...obj };
@@ -8,4 +10,19 @@ export const cleanObject = (obj) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+  return debouncedValue;
 };

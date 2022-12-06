7875ccfcf92af17242fe09ef27f2e45a6746a97a
YutongSheng <544518449@qq.com>
2022-12-06 20:24:13 +0800

    style: 安装emotion，优化登录注册界面

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "context/auth-context";
 import React from "react";
-import { Form, Input, Button } from "antd";
+import { Form, Input } from "antd";
+import { LongButton } from "unauthenticated-app";
 export const RegisterScreen = () => {
   const { register } = useAuth();
   const handleSubmit = (values: { username: string; password: string }) => {
@@ -21,9 +22,9 @@ export const RegisterScreen = () => {
         <Input placeholder="密码" type={"password"} id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit" type={"primary"}>
+        <LongButton htmlType="submit" type={"primary"}>
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );

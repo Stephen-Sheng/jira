8356998704bf01740be3862c3eb1e9eae43e8d9f
YutongSheng <544518449@qq.com>
2022-12-15 17:56:45 +0800

    fix: 修复用户能看到其他用户数据的问题

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -5,6 +5,7 @@ import { http } from "utils/http";
 import { useMount } from "utils";
 import { useAsync } from "utils/use-async";
 import { FullPageErrorCallBack, FullPageLoading } from "components/lib";
+import { useQueryClient } from "react-query";
 
 type AuthForm = {
   username: string;
@@ -41,9 +42,14 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     run,
     setData: setUser,
   } = useAsync<User | null>();
+  const queryClient = useQueryClient();
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
-  const logout = () => auth.logout().then(() => setUser(null));
+  const logout = () =>
+    auth.logout().then(() => {
+      setUser(null);
+      queryClient.clear();
+    });
 
   useMount(() => {
     run(bootstrapUser());
